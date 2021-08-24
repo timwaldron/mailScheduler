60c4fbeb5d96fb0ee9a1146993bd56bae05e1c72
Timothy Waldron <tim@waldron.im>
2021-08-25 09:15:36 +1000

    Made adjustments to forms / Added HTTP GET/POST to backend

--- mailScheduler.php
+++ mailScheduler.php
@@ -5,7 +5,7 @@
  * @author Timothy Waldron <tim@waldrons.com.au>
  * @copyright 2021 Timothy Waldron <https://www.waldrons.com.au>
  * @license AGPL v3
- * @version 4.0.4-beta1
+ * @version 1.0.0
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -23,4 +23,161 @@ class mailScheduler extends PluginBase
   protected $storage = 'DbStorage';
   static protected $name = 'mailScheduler';
   static protected $description = 'Set up and configure survey reminders per participant';
+
+  /**
+   * Initial function, used to subscribe to events
+   */
+  public function init()
+  {
+    /* Adding link in the survey menu */
+    $this->subscribe('beforeToolsMenuRender');
+    
+    // App()->setFlashMessage('Welcome to the mailScheduler plugin!');
+  }
+
+  /**
+   * Rendering the item into the Tools submenu when in a survey config screen
+   */
+  public function beforeToolsMenuRender()
+  {
+    $event = $this->getEvent();
+    $surveyId = $event->get('surveyId');
+
+    $aMenuItem = array(
+      'label' => 'Mail Scheduler',
+      'iconClass' => 'fa fa-envelope-square',
+      'href' => Yii::app()->createUrl(
+        'admin/pluginhelper',
+        array(
+          'sa' => 'sidebody',
+          'plugin' => get_class($this),
+          'method' => 'actionSettings',
+          'surveyId' => $surveyId
+        )
+      ),
+    );
+
+    // Check for old LimeSurvey
+    $menuItem = class_exists("\LimeSurvey\Menu\MenuItem") ? new \LimeSurvey\Menu\MenuItem($aMenuItem) : new \ls\menu\MenuItem($aMenuItem);
+
+    $event->append('menuItems', array($menuItem));
+  }
+
+  /**
+   * Main Function
+  */
+  public function actionSettings($surveyId)
+  {
+    $oSurvey = Survey::model()->findByPk($surveyId);
+    $aData = array();
+
+    if (!$oSurvey)
+    {
+      throw new CHttpException(404, 'This survey does not seem to exist.');
+    }
+
+    if (!Permission::model()->hasSurveyPermission($surveyId, 'surveysettings', 'update'))
+    {
+      throw new CHttpException(403);
+    }
+
+    // Get all the participants in a survey
+    $aData['users'] = Yii::app()->db->createCommand('SELECT token, firstname, lastname FROM {{tokens_' . $surveyId . '}}')->queryAll();
+    $aData['surveyId'] = $surveyId;
+
+    // Create URLs to call the functions that ping the backend
+    $aData['getSchedulerURI'] = Yii::app()->createUrl('admin/pluginhelper', array('plugin' => $this->getName(), 'sa'=>'sidebody', 'method'=>'getScheduleData'));
+    $aData['saveSchedulerURI'] = Yii::app()->createUrl('admin/pluginhelper', array('plugin' => $this->getName(), 'sa'=>'sidebody', 'method'=>'saveScheduleData'));
+
+    $content = $this->renderPartial('settings', $aData, true);
+
+    return $content;
+  }
+
+  /**
+   * Function to get the user schedule data
+   */
+  public function getScheduleData()
+  {
+    $token = $_GET['token'];
+    $surveyId = $_GET['surveyId'];
+
+    $response = $this->httpGet(array('token'=>$token, 'surveyId'=>$surveyId));
+  
+    echo $response;
+  }
+
+  /**
+   * Function to save the user schedule data
+   */
+  public function saveScheduleData()
+  {
+    // Variabless from the HTML form submit
+    $token = $_GET['token'];
+    $surveyId = $_GET['surveyId'];
+
+    // Get all users
+    $users = Yii::app()->db->createCommand('SELECT token, firstname, lastname, email FROM {{tokens_' . $surveyId . '}}')->queryAll();
+
+    // Create the payload
+    $userScheduleModel = array();
+
+    // Find user from list of users and create user schedule model to be sent to the backend
+    foreach ($users as $user)
+    {
+      if ($user['token'] == $token)
+      {
+        $userScheduleModel = $user;
+        $userScheduleModel['surveyId'] = $surveyId;
+      }
+    }
+
+    $userScheduleModel['recruitmentDate'] = $_GET['formData']['recruitmentDate'];
+    $userScheduleModel['surgeryDate'] = $_GET['formData']['surgeryDate'];
+    $userScheduleModel['recalcFollowupDates'] = $_GET['formData']['recalcFollowupDates'];
+    $userScheduleModel['followupDates'] = $_GET['formData']['followupDates'];
+
+    $this->httpPost($userScheduleModel);
+  }
+
+  /**
+   * Send a POST request to the backend API
+   */
+  private function httpPost($postData)
+  {
+    // Create a new cURL resource
+    $ch = curl_init('http://localhost:5000/scheduler');
+    $payload = json_encode($postData);
+
+    // Set the request options
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    // Execute the POST request
+    $response = curl_exec($ch);
+
+    // Close cURL resource
+    curl_close($ch);
+
+    // Echo response
+    echo $response;
+  }
+
+  /**
+   * Send a GET request to the backend API
+   */
+  private function httpGet($params)
+  {
+    $postData = http_build_query($params, '', '&');
+    $fullUrl = 'http://localhost:5000/scheduler/' . $params["surveyId"] . '?' . $postData;
+    $fp = fopen(dirname(__FILE__) . '/errorlog.txt', 'w');
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $fullUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $output = curl_exec($ch);
+    curl_close($ch);
+
+    return $output;
+  }
 }
--- views/settings.php
+++ views/settings.php
@@ -1,45 +1,194 @@
 <div class="row">
-    <div class="col-lg-12 content-right">
-        <?php echo CHtml::beginForm();?>
-        <h3 class="clearfix"><?php echo $title ?>
+  <div class="col-lg-12 content-right">
+    <?php echo CHtml::beginForm('', 'POST', array('id'=>'settingsForm'));?>
+
+      <h3 class="clearfix">Mail Schedule Settings
         <div class='pull-right'>
+          <button id="save-btn" style="display: none;" type="button" class="btn btn-primary" onclick="saveScheduleData();">
+            <i class="fa fa-check" aria-hidden="true"></i> Save
+          </button>
+
           <?php
-              echo CHtml::htmlButton('<i class="fa fa-check" aria-hidden="true"></i> '.gT('Save'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'save','class'=>'btn btn-primary'));
-              echo " ";
-              echo CHtml::htmlButton('<i class="fa fa-check-circle-o " aria-hidden="true"></i> '.gT('Save and close'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'redirect','class'=>'btn btn-default'));
-              echo " ";
-              echo CHtml::link(gT('Close'),Yii::app()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)),array('class'=>'btn btn-danger'));
+            echo CHtml::link('Close', Yii::app()->createUrl('admin/survey', array('sa'=>'view', 'surveyid'=>$surveyId)), array('class'=>'btn btn-danger'));
           ?>
         </div>
-        </h3>
-        <?php if($warningString) {
-            echo CHtml::tag("p",array('class'=>'alert alert-warning'),$warningString);
-        } ?>
-        <?php foreach($aSettings as $legend=>$settings) {
-          $this->widget('ext.SettingsWidget.SettingsWidget', array(
-                //'id'=>'summary',
-                'title'=>$legend,
-                //'prefix' => $pluginClass, This break the label (id!=name)
-                'form' => false,
-                'formHtmlOptions'=>array(
-                    'class'=>'form-core',
-                ),
-                'labelWidth'=>6,
-                'controlWidth'=>6,
-                'settings' => $settings,
-            ));
-        } ?>
-        <div class='row'>
-          <div class='col-md-offset-6 submit-buttons'>
-            <?php
-              echo CHtml::htmlButton('<i class="fa fa-check" aria-hidden="true"></i> '.gT('Save'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'save','class'=>'btn btn-primary'));
-              echo " ";
-              echo CHtml::htmlButton('<i class="fa fa-check-circle-o " aria-hidden="true"></i> '.gT('Save and close'),array('type'=>'submit','name'=>'save'.$pluginClass,'value'=>'redirect','class'=>'btn btn-default'));
-              echo " ";
-              echo CHtml::link(gT('Close'),Yii::app()->createUrl('admin/survey',array('sa'=>'view','surveyid'=>$surveyId)),array('class'=>'btn btn-danger'));
-            ?>
-          </div>
+      </h3>
+
+      <div class="form-group setting setting-select">
+        <label class="default control-label col-sm-3" for="usertoken">Select a patient to configure email reminders</label>
+
+        <select id="usertoken" name="usertoken" class="" onchange="getScheduleData();">
+          <option value="">Select Participant</option>
+          <?php
+            foreach ($users as $user) {
+          ?>
+            <option value="<?php echo $user['token'];?>">
+              <?php echo $user['firstname'] . ' ' . $user['lastname'];?>
+            </option>
+          <?php
+            }
+          ?>
+        </select>
+      </div>
+
+      <div id="followupContent">
+        <h3>Dates</h3>
+        <div id="nullParticipant"></div>
+
+        <!-- Recruitment Date -->
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="recruitmentDate">Recruitment Date</label>
+          <input type="date" id="recruitmentDate" name="recruitmentDate" value="">
         </div>
-        <?php echo CHtml::endForm();?>
-    </div>
+
+        <!-- Surgery Date -->
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="surgeryDate">Surgery Date</label>
+          <input type="date" id="surgeryDate" name="surgeryDate" value="">
+        </div>
+
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="recalcFollowupDates">Recalculate Follow-up Dates?</label>
+          <input type="checkbox" id="recalcFollowupDates" name="recalcFollowupDates">
+        </div>
+        
+        <!-- Followup Dates -->
+        <h3>Follow Up Dates</h3>
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp0">Follow Up: 1</label>
+          <input type="date" id="followUp0" name="followUp0" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp1">Follow Up: 2</label>
+          <input type="date" id="followUp1" name="followUp1" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp2">Follow Up: 3</label>
+          <input type="date" id="followUp2" name="followUp2" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp3">Follow Up: 4</label>
+          <input type="date" id="followUp3" name="followUp3" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp4">Follow Up: 5</label>
+          <input type="date" id="followUp4" name="followUp4" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp5">Follow Up: 6</label>
+          <input type="date" id="followUp5" name="followUp5" value="">
+        </div>
+        
+        <div class="form-group setting setting-select">
+          <label class="default control-label col-sm-3" for="followUp6">Follow Up: 7</label>
+          <input type="date" id="followUp6" name="followUp6" value="">
+        </div>
+      </div>
+
+    <?php echo CHtml::endForm();?>
+  </div>
 </div>
+
+<script>
+  function getScheduleData() {
+    const userToken = document.getElementById('usertoken').value;
+
+    if (userToken) {
+      document.getElementById('save-btn').style.display = 'inline-block';
+    }
+
+    $.ajax({
+      url: '<?php echo $getSchedulerURI ?>',
+      type: 'GET', 
+      data: { 
+        token: userToken,
+        surveyId: '<?php echo $surveyId ?>',
+      },
+      success: function(response) {
+        // Yes, there is a more elegant way of doing this, but timelines etc.
+        let payload = undefined;
+        document.getElementById('nullParticipant').innerHtml = '';
+
+        try {
+          payload = JSON.parse(response.split('<!DOCTYPE html>')[0]); // Split by end-of-payload & start of HTML
+        } catch (e) {
+          console.log('Cannot find data for token/survey: <?php echo $_POST['usertoken'] ?> / <?php echo $surveyId ?>');
+          document.getElementById('nullParticipant').innerHtml = '<h4><strong>Cannot find data for this participant in scheduling system, you can set and save it in this screen.</strong></h4>';
+        }
+        
+        if (!payload) {
+          payload = {
+            recruitmentDate: '',
+            surgeryDate: '',
+            recalcFollowupDates: false,
+            followupDates: [
+              '', // 6 Weeks
+              '', // 3 Months
+              '', // 6 Months
+              '', // 12 Months
+              '', // 2 Years
+              '', // 5 Years
+              '', // 10 Years
+            ],
+          };
+        }
+        
+        // Setting form data values
+        document.getElementById('recruitmentDate').value = payload['recruitmentDate'];
+        document.getElementById('surgeryDate').value = payload['surgeryDate'];
+        document.getElementById('recalcFollowupDates').checked = false;
+
+        // Followup dates
+        let index = 0;
+        for (let date of payload['followupDates']) {
+          document.getElementById('followUp' + index).value = payload['followupDates'][index];
+          index++;
+        }
+      },
+      error: function(xhr) {
+        document.getElementById('followupContent').innerHTML = 'Failed: <pre>' + JSON.stringify(xhr) + '</pre>';
+      }
+    });
+  }
+  
+  function saveScheduleData() {
+    $.ajax({
+      url: '<?php echo $saveSchedulerURI ?>',
+      type: 'GET', 
+      data: {
+        token: document.getElementById('usertoken').value,
+        surveyId: '<?php echo $surveyId ?>',
+        formData: {
+          recruitmentDate: document.getElementById('recruitmentDate').value,
+          surgeryDate: document.getElementById('surgeryDate').value,
+          recalcFollowupDates: document.getElementById('recalcFollowupDates').checked,
+          followupDates: [
+            document.getElementById('followUp0').value, // 6 Weeks
+            document.getElementById('followUp1').value, // 3 Months
+            document.getElementById('followUp2').value, // 6 Months
+            document.getElementById('followUp3').value, // 12 Months
+            document.getElementById('followUp4').value, // 2 Years
+            document.getElementById('followUp5').value, // 5 Years
+            document.getElementById('followUp6').value, // 10 Years
+          ]
+        },
+      },
+      success: function(response) {
+        // TODO: Display notification that save was successful!
+
+        if (document.getElementById('recalcFollowupDates').checked === true) {
+          getScheduleData(); // Reload form data with recalculated dates
+        }
+      },
+      error: function(xhr) {
+        // TODO: Display notification that save was successful!
+      }
+    });
+  }
+</script>
